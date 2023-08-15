18d33c4dbc7a09f5c5065c431022b624ad109e1a
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-08-15 18:50:29 +0000

    Add code for the functions add, contains and to_string

--- 1. DinoSet/main.cpp
+++ 1. DinoSet/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <iomanip>
 #include "dinoset.h"
 
 int main()
 {
+    DinoSet d;
+    d.add(DinoId::tyrannosaurus);
+    d.add(DinoId::velociraptor);
+    d.add(DinoId::pteranodon);
+    std::cout << std::boolalpha;
+    std::cout << d.contains(DinoId::tyrannosaurus) << "\n";
+    std::cout << d.contains(DinoId::stegosaurus) << "\n";
+    std::cout << d.to_string() << "\n";
     return 0;
 }
