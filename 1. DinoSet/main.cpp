c1709a18d5d6692e372ea6fa3fb2ebeff69bdb36
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-08-18 18:21:04 +0000

    Add code for function operator+

--- 1. DinoSet/main.cpp
+++ 1. DinoSet/main.cpp
@@ -4,13 +4,24 @@
 
 int main()
 {
+    DinoSet a;
+    a.add(DinoId::stegosaurus);
+    a.add(DinoId::tyrannosaurus);
+    a.add(DinoId::velociraptor);
+
     DinoSet d;
     d.add(DinoId::tyrannosaurus);
     d.add(DinoId::velociraptor);
     d.add(DinoId::pteranodon);
-    std::cout << std::boolalpha;
-    std::cout << d.contains(DinoId::tyrannosaurus) << "\n";
-    std::cout << d.contains(DinoId::stegosaurus) << "\n";
-    std::cout << d.to_string() << "\n";
+
+    DinoSet c = a + d;
+
+    // std::cout << std::boolalpha;
+    // std::cout << d.contains(DinoId::tyrannosaurus) << "\n";
+    // std::cout << d.contains(DinoId::stegosaurus) << "\n";
+    // std::cout << d.to_string() << "\n";
+
+    std::cout << c.to_string() << "\n";
+
     return 0;
 }
