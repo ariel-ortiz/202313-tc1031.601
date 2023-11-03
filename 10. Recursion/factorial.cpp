fe0c43cb7ce34a6952dbef888387107adc61a40c
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-11-03 17:47:24 +0000

    Add code to implement recursive factorial

---
+++ 10. Recursion/factorial.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+std::ostream&
+operator<<( std::ostream& dest, __int128_t value )
+{
+    std::ostream::sentry s( dest );
+    if ( s ) {
+        __uint128_t tmp = value < 0 ? -value : value;
+        char buffer[ 128 ];
+        char* d = std::end( buffer );
+        do
+        {
+            -- d;
+            *d = "0123456789"[ tmp % 10 ];
+            tmp /= 10;
+        } while ( tmp != 0 );
+        if ( value < 0 ) {
+            -- d;
+            *d = '-';
+        }
+        int len = std::end( buffer ) - d;
+        if ( dest.rdbuf()->sputn( d, len ) != len ) {
+            dest.setstate( std::ios_base::badbit );
+        }
+    }
+    return dest;
+}
+
+__int128 factorial(__int128 n)
+{
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main()
+{
+    __int128 n = 24;
+    std::cout << "factorial(" << n << ") = " << factorial(n) << "\n";
+    std::cout << "sizeof(n) = " << sizeof(n) << "\n";
+
+    return 0;
+}
