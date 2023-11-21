64fc3e4d7d0847decca49cfb689e6d228d139cff
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-11-21 18:53:46 +0000

    Add code to complete BST implementation

--- 11. Binary Trees/treeset/main.cpp
+++ 11. Binary Trees/treeset/main.cpp
@@ -4,22 +4,36 @@
 
 void print_value(int value)
 {
-    std::cout << value << "\n";
+    std::cout << value << " ";
 }
 
-static int resultado = 0;
+int resultado = 0;
 
 void suma(int value)
 {
     resultado += value;
 }
 
+std::vector<int> global_vector;
+
+void add_to_vector(int value)
+{
+    global_vector.push_back(value);
+}
+
 int main()
 {
     TreeSet<int> a {10, 5, 15, 2, 7, 11, 20};
-    a.inorder(print_value);
+    // TreeSet<int> a {10, 15, 20, 25, 17, 18};
+    // a.inorder(print_value);
+    // std::cout << "\n";
+    // a.inorder(suma);
+    // std::cout << resultado << "\n";
+    a.levelorder(add_to_vector);
+    for (int x : global_vector) {
+        std::cout << x << " ";
+    }
     std::cout << "\n";
-    a.inorder(suma);
-    std::cout << resultado << "\n";
+    std::cout << "is_perfect = " << a.is_perfect() << "\n";
     return 0;
 }
